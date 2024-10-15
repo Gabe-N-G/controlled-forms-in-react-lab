0412e2e4bdee7d5bfcd8b57911cb84a3cef96c4e
g g <gabedotgutierrez@gmail.com>
2024-10-14 21:01:47 -0400

    All things working, key error in console

--- components/bookshelf/Bookshelf.jsx
+++ components/bookshelf/Bookshelf.jsx
@@ -15,15 +15,15 @@ const Bookshelf = () => {
     ])  
 
     const handleInputChange = (e) =>{
-        console.log(e.target.value)
+        console.log(e.target.name, e.target.value)
         setNewBook({...newBook, [e.target.name]: e.target.value})
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(newBook)
-        setBooks({...books, newBook})
-        setNewBook({title: "", author: ""})
+        setBooks([...books, newBook])
+        setNewBook({title: " ", author: " "})
     }
 
     return  (
@@ -37,22 +37,26 @@ const Bookshelf = () => {
                         id='title'
                         name='title'
                         onChange={handleInputChange}
+                        value={newBook.title}
                     ></input>
                     <label htmlFor="author">Author</label>
                     <input 
                         id='author'
                         name='author'
                         onChange={handleInputChange}
+                        value={newBook.author}
                     ></input>
                     <button type='submit'>Add book</button>
                 </form>
             </div>
 
             <div className="bookCardsDiv">{/* Book cards will display here */}
-                {books.map(book =>
+                {books.map((book,i)=>
                     <>
-                        <p>Title: {book.title}</p>
-                        <p>Author: {book.author}</p>
+                        <div className='bookCard' key={i}>
+                            <h3>Title: {book.title}</h3>
+                            <p>Author: {book.author}</p>
+                        </div>
                     </>
                 )}
             </div>
